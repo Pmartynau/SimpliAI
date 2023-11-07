ace809cb06fb5579c50c510f1cd3d71d513a7369
Pavel <pashamartynau@gmial.com>
2023-11-06 16:02:23 -0800

    pricing

--- app/(dashboard)/(routes)/pricing/page.tsx
+++ app/(dashboard)/(routes)/pricing/page.tsx
@@ -1,9 +1,21 @@
+'use client'
 import React from 'react'
+import Link from 'next/link'
 
 const Pricing = () => {
   return (
-    <div>Pricing Accordion</div>
+    <><div className='mx-auto mb-10 sm:max-w-lg'>
+      <h1 className='text-6xl font-bold sm:text-7xl'>
+        Pricing
+      </h1>
+      <p className='mt-5 text-gray-600 sm:text-lg'>
+        Whether you&apos;re just trying out our service
+        or need more, we&apos;ve got you covered.
+      </p>
+    </div><div className='p-5'>
+ hello world
+      </div></>
   )
 }
 
-export default Pricing
+  export default Pricing
