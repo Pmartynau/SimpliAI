6926005934008698934aa408b2d4bda32580e3f8
pmartynau <martyp@uw.edu>
2023-11-22 01:11:24 -0800

    adding new font badge/new api key

--- components/new-modal.tsx
+++ components/new-modal.tsx
@@ -1,9 +1,9 @@
 import { Sparkles } from "lucide-react";
-
+import './styles/fonts.css';
 export const NewModal = () => {
   return ( 
     <div className=" mx-4 flex justify-center items-center">
-      <span className="text-red-500 font-bold animate-bounce">New</span>
+      <span style={{fontFamily: 'CustomFont'}}className="text-red-500 font-bold animate-bounce">New</span>
       <Sparkles className="w-5 h-5 text-yellow-500 animate-spin-slow" />
     </div>
   );
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -51,14 +51,15 @@ const routes = [
     label: "Vision",
     icon: Eye,
     href: "/vision",
-    color: "text-blue-400"
+    color: "text-blue-400",
+    isNew: true
   },
   {
     label: "Ask an Expert",
     icon: VenetianMask,
     href: "/alter",
     color: "text-amber-500",
-    isNew: true,
+    //isNew: true,
   },
   {
     label: "Image Generator",
@@ -89,7 +90,7 @@ const routes = [
     icon: FaToolbox,
     href: "/bookmarks",
     color: "text-white",
-    isNew: true,
+   // isNew: true,
   },
   {
     label: "Settings",
