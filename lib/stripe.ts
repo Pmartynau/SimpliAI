457e51eed194752655018a07a5d0a473c9162694
pmartynau <martyp@uw.edu>
2023-11-27 16:04:25 -0800

    stripe.ts error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,18 +4,3 @@ export const stripe = new Stripe(process.env.STRIPE_API_KEY!,{
   apiVersion: "2022-11-15",
   typescript: true,
 });
-
-export const createCheckoutSession = async (items) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: items,
-    mode: "payment",
-    success_url: "YOUR_SUCCESS_URL",
-    cancel_url: "YOUR_CANCEL_URL",
-    discounts: [{
-      coupon: 'YOUR_COUPON_CODE',
-    }],
-  });
-
-  return 
-};
