83d0b6de58d6e1f926669733c90039571eae00fc
Paul Martynau <paul@Pauls-MacBook-Pro.local>
2024-09-02 13:33:55 -0700

    fix x build error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = async () => {
       
       <div className="flex w-full space-x-2  flex-row-1 justify-end">
        <a className="pt-2 pl-3" href="https://www.buymeacoffee.com/pashamartyl"><Coffee className="h-6 w-6" /></a>
-       <a className="pt-2 pl-3" href="https://twitter.com/SimpliAIApp"><Image width={24} height={24} src="/x-twitter.svg"/></a>  
+       <a className="pt-2 pl-3" href="https://twitter.com/SimpliAIApp"><Image width={12} height={12} alt="Twitter icon" src="/x-twitter.svg"/></a>  
        <DarkMode/>
         <UserButton afterSignOutUrl="/"/>
       </div>
